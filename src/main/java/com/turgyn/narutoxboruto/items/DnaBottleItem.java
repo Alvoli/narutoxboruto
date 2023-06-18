4d09c5dea1215868e2a3492147e254ace7f16af8
lgow <@>
2023-06-17 21:02:33 -0400

    Retextured some stuff etc

--- src/main/java/com/turgyn/narutoxboruto/items/DnaBottleItem.java
+++ src/main/java/com/turgyn/narutoxboruto/items/DnaBottleItem.java
@@ -5,31 +5,48 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
+
+import static com.turgyn.narutoxboruto.items.ModItems.*;
 
 public class DnaBottleItem extends Item {
 	public DnaBottleItem(Properties properties) {
 		super(properties);
 	}
 
-	public void giveKG(RandomSource random) {
+	public void giveLoot(LivingEntity pLivingEntity) {
 		//calculate KG chance here
-		random.nextInt();
+		// which KG
+		//calculate Nature release chance
+		//which Nature
+		//todo fix item replacement
+		if (pLivingEntity.getRandom().nextBoolean()) { //50%
+			if (pLivingEntity instanceof Player player) {
+				int rand = player.getRandom().nextInt(12);
+				Item stack = null;
+				switch (rand) {
+					case 0, 1 -> stack = EARTH_RELEASE.get();
+					case 2, 3 -> stack = FIRE_RELEASE.get();
+					case 4, 5 -> stack = LIGHTNING_RELEASE.get();
+					case 6, 7 -> stack = WATER_RELEASE.get();
+					case 8, 9 -> stack = WIND_RELEASE.get();
+					case 10 -> stack = YIN_RELEASE.get();
+					case 11 -> stack = YANG_RELEASE.get();
+				}
+				player.addItem(stack.getDefaultInstance());
+			}
+		}
 	}
 
 	@Override
 	public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pLivingEntity) {
-		this.giveKG(pLivingEntity.getRandom());
+		this.giveLoot(pLivingEntity);
 		return this.drink(pStack, pLevel, pLivingEntity);
 	}
 
